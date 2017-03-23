d4665abd24c93a17dc5e5b013114beb324caf9a0
Théo Mathieu <theo.mathieu@bhvr.com>
2017-03-22 22:04:02 -0400

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,72 @@
+import { Component, HostListener } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+
+  public row1 = [
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+  ];
+  public row2 = [
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png', kneel: true},
+    {image: '/assets/img/cards/character.png'},
+  ];
+  public row3 = [
+    {image: '/assets/img/cards/character.png', kneel: true},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+    {image: '/assets/img/cards/character.png'},
+  ];
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.onResize();
+  }
+
+  @HostListener('window:resize') public onResize() {
+    console.log(document.body.clientHeight);
+  }
+
+  public getLeft(row, i) {
+    const totalHeight = document.body.clientHeight;
+    const cardHeight = totalHeight / 6 - 10;
+    const cardWidth = (cardHeight * 300) / 418;
+
+    const cardsBefore = row.filter((card, index) => index < i);
+    const cardsKneeledBefore = cardsBefore.filter((c) => c.kneel);
+    const cardsNotKneeledBefore = cardsBefore.filter((c) => !c.kneel);
+
+    let left = cardWidth * cardsNotKneeledBefore.length + cardHeight * cardsKneeledBefore.length;
+    if (row[i].kneel) {
+      left += ((cardHeight - cardWidth) / 2);
+    }
+    left += i * 2;
+
+    return 'calc(100% - ' + (left + cardWidth) + 'px)';
+  }
+
+  public switchKneel(row, i) {
+    row[i].kneel = !row[i].kneel;
+  }
+}
